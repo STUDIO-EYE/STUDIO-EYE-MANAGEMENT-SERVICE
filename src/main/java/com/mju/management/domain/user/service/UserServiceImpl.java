51725bf3c902551e7aecf59e239011a08ebedddf
ibaesuyeon <ibaesuyeon@Naver.com>
2024-03-31 18:18:10 +0900

    [Feat] 로그인 기능 구현
    주석 해제 및 환경설정

--- src/main/java/com/mju/management/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/mju/management/domain/user/service/UserServiceImpl.java
@@ -28,29 +28,29 @@ import java.util.List;
 @Slf4j
 public class UserServiceImpl {
 
-//    private final UserFeignClient userFeignClient;
+    private final UserFeignClient userFeignClient;
 
     public GetUserResponseDto getUser(Long userId){
-//        try {return userFeignClient.getUser(userId).getBody();}
-//        catch (Exception e){return null;}
-        return null;
+        try {return userFeignClient.getUser(userId).getBody();}
+        catch (Exception e){return null;}
+//        return null;
     }
 
     public String getUsername(Long userId){
-//        GetUserResponseDto getUserResponseDto = null;
-//        try{
-//            getUserResponseDto = userFeignClient.getUser(userId).getBody();
-//        }catch (FeignException.InternalServerError e){
-//            e.printStackTrace();
-//            return "(내부 서버 오류)";
-//        }catch (RetryableException e){
-//            e.printStackTrace();
-//            return "(응답 시간 초과)";
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            return "(알 수 없음)";
-//        }
-//        return getUserResponseDto.getName();
-        return null;
+        GetUserResponseDto getUserResponseDto = null;
+        try{
+            getUserResponseDto = userFeignClient.getUser(userId).getBody();
+        }catch (FeignException.InternalServerError e){
+            e.printStackTrace();
+            return "(내부 서버 오류)";
+        }catch (RetryableException e){
+            e.printStackTrace();
+            return "(응답 시간 초과)";
+        }catch (Exception e){
+            e.printStackTrace();
+            return "(알 수 없음)";
+        }
+        return getUserResponseDto.getName();
+//        return null;
     }
 }
